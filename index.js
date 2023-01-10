cc37b76b25467fa1de3479ecd7ddd0e11c4bc5c5
Iryna Velychko <76870532+IrinaV713@users.noreply.github.com>
2023-01-10 17:12:27 +0200

    change app loading and reorganize files

--- index.js
+++ index.js
@@ -2,17 +2,20 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static('src'));
+
 app.get('/', function(req, res) {
-    res.sendFile('index.html', {root: __dirname })
+    res.send('Application started.')
 });
 
 const server = app.listen(port, () => {
     console.log(`Listening on port ${port}`); 
+    console.log(`Go to http://localhost:${port}`);
 });
 
 process.on('SIGTERM', () => {
     server.close(() => {
-        console.log('HTTP server closed.');     
+        console.log('Application closed.');     
         process.exit(0);
     });
 });
