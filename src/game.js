034228a0ebcf39f02771482a332776c5f10d6971
Iryna Velychko <76870532+IrinaV713@users.noreply.github.com>
2023-01-11 23:02:46 +0200

    refactoring

--- src/game.js
+++ src/game.js
@@ -20,16 +20,7 @@ function initializeField() {
     }
 }
 
-/*function resetGrids() {
-    for (var i = 0; i < rows; i++) {
-        for (var j = 0; j < cols; j++) {
-            grid[i][j] = 0;
-            nextGrid[i][j] = 0;
-        }
-    }
-}*/
-
-function copyAndResetGrid() {
+function passFieldState() {
     for (var i = 0; i < rows; i++) {
         for (var j = 0; j < cols; j++) {
             grid[i][j] = next[i][j];
@@ -41,13 +32,12 @@ function copyAndResetGrid() {
 function init() {
     createTable();
     initializeField();
-    //resetGrids();
-    setupControlButtons();
+    buttonSetup();
 }
 
 
 function createTable() {
-    var gridElement = document.getElementById('grid');
+    var fieldElement = document.getElementById('field');
     var table = document.createElement("table");
     for (var i = 0; i < rows; i++) {
         var tr = document.createElement("tr");
@@ -55,15 +45,15 @@ function createTable() {
             var cell = document.createElement("td");
             cell.setAttribute("id", i + "-" + j);
             cell.setAttribute("class", "dead");
-            cell.onclick = changeState;
+            cell.onclick = changeCellState;
             tr.appendChild(cell);
         }
         table.appendChild(tr);
     }
-    gridElement.appendChild(table);
+    fieldElement.appendChild(table);
 }
 
-function changeState() {
+function changeCellState() {
     var coords = this.id.split("-");
     var x = coords[0];
     var y = coords[1];
@@ -91,7 +81,7 @@ function updateField() {
     }
 }
 
-function setupControlButtons() {
+function buttonSetup() {
     var startButton = document.getElementById('start');
     startButton.onclick = startButtonHandler;
     
@@ -132,17 +122,6 @@ function clearButtonHandler() {
             next[i][j] = 0;
         }
     }
-
-    /*var cellsList = document.getElementsByClassName("live");
-    var cells = [];
-    for (var i = 0; i < cellsList.length; i++) {
-        cells.push(cellsList[i]);
-    }
-    
-    for (var i = 0; i < cells.length; i++) {
-        cells[i].setAttribute("class", "dead");
-    }*/
-    //resetGrids;
 }
 
 function startButtonHandler() {
@@ -170,7 +149,7 @@ function nextGeneration() {
             cellNextState(i, j);
         }
     }
-    copyAndResetGrid();
+    passFieldState();
     updateField();
 }
 
@@ -195,7 +174,6 @@ function cellNextState(x, y) {
     
 function getAliveNeighbors(x, y) {
     var count = 0;
-
     for(var i = x; i <= x+2; i++){
         for(var j = y; j <= y+2; j++){
             if(grid[(i - 1 + rows) % rows][(j - 1 + cols) % cols] == 1){
@@ -205,30 +183,6 @@ function getAliveNeighbors(x, y) {
             }
         }
     }
-    /*if (x-1 >= 0) {
-        if (grid[x-1][y] == 1) count++;
-    }
-    if (x-1 >= 0 && y-1 >= 0) {
-        if (grid[x-1][y-1] == 1) count++;
-    }
-    if (x-1 >= 0 && y+1 < cols) {
-        if (grid[x-1][y+1] == 1) count++;
-    }
-    if (y-1 >= 0) {
-        if (grid[x][y-1] == 1) count++;
-    }
-    if (y+1 < cols) {
-        if (grid[x][y+1] == 1) count++;
-    }
-    if (x+1 < rows) {
-        if (grid[x+1][y] == 1) count++;
-    }
-    if (x+1 < rows && y-1 >= 0) {
-        if (grid[x+1][y-1] == 1) count++;
-    }
-    if (x+1 < rows && y+1 < cols) {
-        if (grid[x+1][y+1] == 1) count++;
-    }*/
     return count;
 }
 
